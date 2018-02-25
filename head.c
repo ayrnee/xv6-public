7383bb7cf336ed33ae0d00942e559a809faaa64e
Ernest Gottschalk <ayrnee@gmail.com>
2018-02-25 12:02:07 -0500

    updates

--- head.c
+++ head.c
@@ -52,7 +52,8 @@ int main(int argc, char* argv[]){
   else{
     if ( argc == 2 && argv[1][0] == '-' ){
       printf(1,"%s\n",argv[1]);
-      printf(1,"%d\n",atoi(argv[1][1]));
+      int val = atoi(argv[1]);
+      printf(1,"%d\n",val);
       head(0,atoi(argv[1]));
       exit();
     }
