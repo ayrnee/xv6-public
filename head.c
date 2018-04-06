82fffa010926cd5ee0b1f06505ef35b65845e4b4
Ernest Gottschalk <ayrnee@gmail.com>
2018-04-06 14:41:01 -0400

    stashing

--- head.c
+++ head.c
@@ -82,7 +82,6 @@ int main(int argc, char* argv[]){
       close(fd);
       exit();
     }
-
   }
   exit();
 }
--- hello.c
+++ hello.c
@@ -4,7 +4,6 @@
 
 int main(int argc, char* argv[])
 {
-  // printf(1, "Hello, World!");
-  printf(1, "%s\n",argv[1]);
+  printf(1, "Hello, World!");
   exit();
 }
